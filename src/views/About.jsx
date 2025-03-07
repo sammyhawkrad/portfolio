344c012d4a21fd663052bb095848f2913f4c0c34
Samuel Darkwah Manu <manusam14@yahoo.com>
2025-03-07 14:02:03 +0100

    Move hardcoded about details to data.json

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -15,28 +15,25 @@ export default function About() {
             <section>
                 <div>
                     <h3 className="font-medium underline underline-offset-8 py-4">Education</h3>
-                    <p className="text-gray-600 dark:text-gray-400 pb-2">MSc. Cartography, TU Munich | TU Wien | TU Dresden | UTwente, Germany, Austria &amp; The Netherlands (in pursuit)</p>
-                    <p className="text-gray-600 dark:text-gray-400 pb-2">BSc. Development Planning, Kwame Nkrumah University of Science and Technology, Ghana</p>
+                    {data.education.map((item, index) => (
+                        <p key={index} className="text-gray-600 dark:text-gray-400 pb-2">{item}</p>
+                    ))
+                    }
                 </div>
                 <div></div>
             </section>
             <section className="md:flex md:flex-col md:gap-10 md:mt-10">
                 <div className="md:flex md:justify-start md:items-start md:gap-20">
                     <h3 className="font-medium underline underline-offset-8 py-4 md:pt-0 md:min-w-[11rem]">Programming Languages</h3>
-                    <p className="text-gray-600 dark:text-gray-400">JavaScript, TypeScript, Python, Java, HTML, CSS, SCSS, SQL </p>
+                    <p className="text-gray-600 dark:text-gray-400">{data.programming_languages.join(", ")}</p>
                 </div>
                 <div className="md:flex justify-start items-start gap-20">
                     <h3 className="font-medium underline underline-offset-8 py-4 md:pt-0 md:w-[11rem]">Tools &amp; Technologies</h3>
                     <div>
-                        <p className="text-gray-600 dark:text-gray-400">
-                            <span className="italic">Web Development</span> - Vue, React, Node, Express, FastAPI, Git, Tailwind, Bootstrap, MongoDB, PostreSQL
-                        </p>
-                        <p className="text-gray-600 dark:text-gray-400 my-2">
-                            <span className="italic">Cartography & GIS</span> - QGIS, ArcGIS, Leaflet, Geoserver, PostGIS
-                        </p>
-                        <p className="text-gray-600 dark:text-gray-400">
-                            <span className="italic">Data Science</span> - Power BI, Tableau, Pandas, Numpy, Matplotlib, Seaborn, Scikit-learn, MySQL, SQL Server
-                        </p>
+                        {data.tools_tech.map((item, index) => (
+                            <p key={index} className="text-gray-600 dark:text-gray-400">
+                            <span className="italic">{item.group}</span> - {item.items.join(", ")}</p>
+                        ))}
                     </div>
                 </div>
             </section>
